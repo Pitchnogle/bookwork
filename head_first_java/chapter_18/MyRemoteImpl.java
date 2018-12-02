1bc51df89925071b2539751cf3954c1e305b039d
Pitchnogle <pitchnogle@gmail.com>
2018-12-01 23:51:21 -0700

    add rmi client. bugfix in server

--- head_first_java/chapter_18/MyRemoteImpl.java
+++ head_first_java/chapter_18/MyRemoteImpl.java
@@ -2,19 +2,20 @@ import java.rmi.*;
 import java.rmi.server.*;
 
 public class MyRemoteImpl extends UnicastRemoteObject implements MyRemote {
-  public static void main(String[] args) {
-    try {
-      MyRemote service = MyRemoteImpl();
-      Naming.rebind("Remote Hello", service);
-    }
-    catch (Exception e) {
-      e.printStackTrace();
-    }
-  }
 
   public String sayHello() {
     return "Server says 'Hey'";
   }
 
   public MyRemoteImpl() throws RemoteException {}
+
+  public static void main(String[] args) {
+    try {
+      MyRemote service = new MyRemoteImpl();
+      Naming.rebind("RemoteHello", service);
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 }
