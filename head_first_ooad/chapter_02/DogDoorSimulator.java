1aff5bd352cc5fe12063bd0695763b63e9364c0d
Pitchnogle <pitchnogle@gmail.com>
2018-12-02 17:02:59 -0700

    update for 2.1 use case

--- head_first_ooad/chapter_02/DogDoorSimulator.java
+++ head_first_ooad/chapter_02/DogDoorSimulator.java
@@ -5,9 +5,21 @@ public class DogDoorSimulator {
 
     System.out.println("Fido barks to go outside...");
     remote.pressButton();
-    
+
     System.out.println("\nFido has gone outside...");
     System.out.println("\nFido's all done...");
+
+    try {
+      Thread.currentThread().sleep(10000);
+    }
+    catch (Exception e) {
+      // do nothing
+    }
+
+    System.out.println("...but he's stuck outside!");
+    System.out.println("\nFido starts barking...");
+    System.out.println("...so Gina grabs the remote control.");
+    remote.pressButton();
     System.out.println("\nFido's back inside");
   }
 }
