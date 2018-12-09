cce0f0daea35f863192b712aabeb040cdd0ed873
Pitchnogle <pitchnogle@gmail.com>
2018-12-08 21:18:03 -0700

    add set/get property for unit class

--- head_first_ooad/chapter_07/Unit.java
+++ head_first_ooad/chapter_07/Unit.java
@@ -1,5 +1,34 @@
+import java.util.HashMap;
+
 public class Unit {
+  private String type;
+  private HashMap properties;
+
   public Unit() {
-    
+
   }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public void setProperty(String property, Object value)
+  {
+    if (properties == null) {
+      properties = new HashMap();
+    }
+    properties.put(property, value);
+  }
+  
+  public Object getProperty(String property) {
+    if (properties == null) {
+      return null;
+    }
+    return properties.get(property);
+  }
+
 }
