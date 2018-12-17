08355c29f237e487ed9c2b41e6d8b89ecd7926b3
Pitchnogle <pitchnogle@gmail.com>
2018-12-16 21:13:54 -0700

    update card counter to work in loop

--- head_first_c/chapter_01/cards.c
+++ head_first_c/chapter_01/cards.c
@@ -2,33 +2,40 @@
 #include <stdlib.h>
 
 int main() {
-  char card_name[3];
-  puts("Enter the card name: ");
-  scanf("%2s", card_name);
+  char card_name[3] = {0};
+  int count = 0;
+  while (card_name[0] != 'x') {
+    puts("Enter the card name: ");
+    scanf("%2s", card_name);
 
-  int val = 0;
-  if (card_name[0] == 'K') {
-    val = 10;
-  }
-  else if (card_name[0] == 'Q') {
-    val = 10;
-  }
-  else if (card_name[0] == 'J') {
-    val = 10;
-  }
-  else if (card_name[0] == 'A') {
-    val = 11;
-  }
-  else {
-    val = atoi(card_name);
-  }
+    int val = 0;
+    switch (card_name[0]) {
+    case 'k': case 'K':
+    case 'q': case 'Q':
+    case 'j': case 'J':
+      val = 10;
+      break;
+    case 'a': case 'A':
+      val = 11;
+      break;
+    case 'x':
+      continue;
+    default:
+      val = atoi(card_name);
+      if (val < 1 || val > 10) {
+        puts("error");
+        continue;
+      }
+      break;
+    }
 
-  // Check if the value is between 3 and 6 (inclusive)
-  if (val >= 3 && val <= 6) {
-    puts("The count has gone up");
-  }
-  else if (val == 10) {
-    puts("The count has gone down");
+    if (val >= 3 && val <= 6) {
+      count++;
+    }
+    else if (val == 10) {
+      count--;
+    }
+    printf("Current count: %i\n", count);
   }
 
   return 0;
