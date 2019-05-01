7190531c8840372183c7cef4c64480202e82b09a
Pitchnogle <pitchnogle@gmail.com>
2019-04-30 20:01:15 -0600

    update with default args and optional indentation

--- head_first_python/chapter_02/nester/nester.py
+++ head_first_python/chapter_02/nester/nester.py
@@ -1,10 +1,13 @@
-def print_lol(the_list):
+def print_lol(the_list, indent = False, level = 0):
   """
   This function prints out a list recursively where each line will contain one
   entry from the list which may be a nested list.
   """
   for each_item in the_list:
     if isinstance(each_item, list):
-      print_lol(each_item)
+      print_lol(each_item, indent, level + 1)
     else:
+      if indent:
+        for spaces in range(level):
+          print("  ", end = '')
       print(each_item)
