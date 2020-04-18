cc5213cd1dd4fd3232202e89552eebe2e940a15b
Pitchnogle <pitchnogle@gmail.com>
2020-04-17 20:48:04 -0600

    add atom

---
+++ c_interfaces_and_implementations/src/atom.h
@@ -0,0 +1,13 @@
+#ifndef ATOM_INCLUDED
+#define ATOM_INCLUDED
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Functions
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+extern       int   Atom_length(const char *str);
+extern const char *Atom_new   (const char *str, int len);
+extern const char *Atom_string(const char *str);
+extern const char *Atom_int   (long n);
+
+#endif
