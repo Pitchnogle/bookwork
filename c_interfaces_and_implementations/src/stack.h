d56a6343728b246e71d4cdb05ee5196c8be316de
Pitchnogle <pitchnogle@gmail.com>
2020-04-17 20:48:46 -0600

    add stack

---
+++ c_interfaces_and_implementations/src/stack.h
@@ -0,0 +1,22 @@
+#ifndef STACK_INCLUDED
+#define STACK_INCLUDED
+
+// -----------------------------------------------------------------------------
+// Definitions
+// -----------------------------------------------------------------------------
+
+#define T Stack_T
+typedef struct T *T;
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Functions
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+extern T     Stack_new  (void);
+extern int   Stack_empty(T stk);
+extern void  Stack_push (T stk, void *x);
+extern void *Stack_pop  (T stk);
+extern void  Stack_free (T *stk);
+
+#undef T
+#endif
