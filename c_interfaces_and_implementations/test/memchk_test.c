58e3d9e3d6615b1f9188134bd4073f6403054e21
Pitchnogle <pitchnogle@gmail.com>
2020-04-20 21:46:33 -0600

    remove mem leak test

--- c_interfaces_and_implementations/test/memchk_test.c
+++ c_interfaces_and_implementations/test/memchk_test.c
@@ -13,12 +13,6 @@
 // Test Functions
 // -----------------------------------------------------------------------------
 
-extern void Mem_leak(void (*apply)(void *ptr, long size, const char *file, int line, void *cl), void *cl);
-
-// -----------------------------------------------------------------------------
-// Test Functions
-// -----------------------------------------------------------------------------
-
 void test_zero_alloc();
 void test_zero_calloc();
 void test_negative_alloc();
@@ -29,15 +23,15 @@ void test_ok_alloc();
 void test_ok_calloc();
 void test_free();
 void test_resize();
-void test_mem_leak();
-
-void inuse(void *ptr, long size, const char*file, int line, void *cl);
 
 // =============================================================================
 // Main Program
 // =============================================================================
 
 #define TEST_COUNT  (100)
+
+// We need to limit this somewhat since there is some minimum span between two
+// addresses. We can get way closer to LONG_MAX, but being lazy...
 #define TEST_TOOBIG (LONG_MAX / 2)
 
 int main()
@@ -52,7 +46,6 @@ int main()
   test_ok_calloc();
   test_free();
   test_resize();
-  test_mem_leak();
 
   return 0;
 }
@@ -319,32 +312,4 @@ void test_resize()
   FREE(x);
 
   print_results("Test RESIZE()", pass);
-}
-
-void test_mem_leak()
-{
-  bool pass = true;
-
-  float *x = NULL;
-
-  TRY
-    x = ALLOC(TEST_COUNT * sizeof (*x));
-  EXCEPT(Mem_Failed)
-    pass = false;
-  END_TRY;
- 
-  printf("Test mem leak: (there should be one leak)\n");
-  Mem_leak(inuse, stdout);
-
-  FREE(x);
-  printf("Test mem leak: (nothing should print after)\n");
-  Mem_leak(inuse, stdout);
-}
-
-void inuse(void *ptr, long size, const char *file, int line, void *cl)
-{
-  FILE *fout = cl;
-
-  fprintf(fout, "** memory in use at %p\n", ptr);
-  fprintf(fout, "This block is %ld bytes long and was allocated from %s:%d\n", size, file, line);
 }
