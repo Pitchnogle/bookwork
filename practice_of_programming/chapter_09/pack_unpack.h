d17b66a19ef00d5ca8dc8298a0e54c9ba6b8926d
Pitchnogle <pitchnogle@gmail.com>
2020-05-05 20:29:25 -0600

    add some docs

--- practice_of_programming/chapter_09/pack_unpack.h
+++ practice_of_programming/chapter_09/pack_unpack.h
@@ -1,3 +1,18 @@
+/*
+Utility to pack/unpack binary data to/from buffer
+
+The **fmt** argument is a string where the characters provided represent the 
+data to pack/unpack:
+- "b" -- int8_t/uint8_t
+- "c" -- char
+- "h" -- int16_t/uint16_t
+- "i" -- int32_t/uint32_t
+- "f" -- float
+- "s" -- char* (c-string)
+
+Since the functions are variadic, the whole point is to be able to pack/unpack
+an arbitrary number of values easily.
+*/
 #ifndef PACK_UNPACK_h
 #define PACK_UNPACK_h
 
@@ -9,12 +24,16 @@
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 /**
+Pack binary items into buffer
 
+@return the length of the packed buffer
 */
 int pack(uint8_t *buf, char *fmt, ...);
 
 /**
+Unpack packed items from buffer
 
+@return the length of data unpacked
 */ 
 int unpack(uint8_t *buf, char *fmt, ...);
 
