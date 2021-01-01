9acd86403ded0008405397fa504859d4db62f8d5
Pitchnogle <pitchnogle@gmail.com>
2020-12-31 18:30:11 -0700

    read chapter 6

---
+++ beginning_c++_though_game_programming/inventory_referencer/inventory_referencer/inventory_referencer.cpp
@@ -0,0 +1,42 @@
+﻿// Chapter 6 -- Inventory Referencer
+// Demonstrates returning a reference
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+string& ref_to_element(vector<string>& inventory, int i);
+
+int main()
+{
+	vector<string> inventory;
+	inventory.push_back("sword");
+	inventory.push_back("armor");
+	inventory.push_back("shield");
+
+	cout << "Sending the returned reference to cout:\n";
+	cout << ref_to_element(inventory, 0) << "\n\n";
+
+	cout << "Assigning the returned reference to another reference.\n";
+	string& r_str = ref_to_element(inventory, 1);
+	cout << "Sending the new reference to cout:\n";
+	cout << r_str << "\n\n";
+
+	cout << "Assigning the returned reference to a string object.\n";
+	string str = ref_to_element(inventory, 2);
+	cout << "Sending the new string object to cout:\n";
+	cout << str << "\n\n";
+
+	cout << "Altering an object through a returned reference.\n";
+	r_str = "Healing potion";
+	cout << "Sending the altered object to cout:\n";
+	cout << inventory[1] << endl;
+
+	return 0;
+}
+
+string& ref_to_element(vector<string>& inventory, int i)
+{
+	return inventory[i];
+}
---
+++ beginning_c++_though_game_programming/referencing/referencing/referencing.cpp
@@ -0,0 +1,28 @@
+﻿// Chapter 6 -- Referencing
+// Demonstrates using references
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int my_score = 1000;
+	int& mikes_score = my_score;
+
+	cout << "my_score is: " << my_score << endl;
+	cout << "mikes_score is: " << mikes_score << endl;
+
+	cout << "Adding 500 to my_score\n";
+	my_score += 500;
+
+	cout << "my_score is: " << my_score << endl;
+	cout << "mikes_score is: " << mikes_score << endl;
+
+	cout << "Adding 500 to mikes_score\n";
+	mikes_score += 500;
+
+	cout << "my_score is: " << my_score << endl;
+	cout << "mikes_score is: " << mikes_score << endl;
+
+	return 0;
+}
